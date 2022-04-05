a2e292cb1dce94253baefb9ca357ba9aa0bb3509
pythagoras58 <solomondanso58@gmail.com>
2022-04-05 22:25:18 +0100

    Add project files.

---
+++ basicLearner/Mathematics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicLearner
+{
+    class Mathematics
+    {
+         static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ basicLearner/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace basicLearner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            string firstName;
+            string age;
+
+            Console.Write("Enter name");
+            firstName = Console.ReadLine();
+            Console.Write("Enter Age");
+            age = Console.ReadLine();
+
+            Console.WriteLine(firstName);
+            Console.WriteLine(age);
+        }
+    }
+}
